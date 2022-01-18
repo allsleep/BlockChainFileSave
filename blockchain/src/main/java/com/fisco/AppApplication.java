e38815a6f67768ea79b2bacb8d527de051d26190
LinHan <>
2022-01-18 22:01:18 +0800

    v0.0.9

--- blockchain/src/main/java/com/fisco/AppApplication.java
+++ blockchain/src/main/java/com/fisco/AppApplication.java
@@ -2,10 +2,10 @@ package com.fisco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
